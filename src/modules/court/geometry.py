2f282fd6c428b7bd27a7c2663976091585462dd0
Iacopo Schianchi <iacopo.schianchi@gmail.com>
2026-08-10 16:30:18 +0200

    Fix get_convex_hull_mask

--- src/modules/court/geometry.py
+++ src/modules/court/geometry.py
@@ -7,15 +7,20 @@ PADDING_PIXELS = 40
 GROUP_THRESH_PX = 20
 
 def get_convex_hull_mask(raw_mask):
-    contours, _ = cv2.findContours(raw_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    mask = raw_mask.astype(np.uint8) * 255
+
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    if not contours:
+        return np.zeros_like(mask)
 
     all_points = np.vstack(contours)
     hull = cv2.convexHull(all_points)
 
-    hull_mask = np.zeros_like(raw_mask)
+    hull_mask = np.zeros_like(mask)
     hull_mask.filLConvexPoly(hull_mask, hull, True)
 
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2 * PADDING_PIXELS + 1, 2 * PADDING_PIXELS + 1))
     padded_mask = cv2.dilate(hull_mask, kernel)
 
-    return padded_mask
+    return padded_mask > 0
