856b45987a74fdd50bc53fadd5db0670be838915
Iacopo Schianchi <iacopo.schianchi@gmail.com>
2026-08-10 14:26:39 +0200

    Change clip to relative path

--- src/app.py
+++ src/app.py
@@ -3,4 +3,4 @@ from processor import VideoProcessor
 FPS = 30
 
 vp = VideoProcessor('p1', 'p2', FPS)
-vp.process(r"c:\Users\iacop\Research Projects\tennis-tactics\assets\clip.mp4")
+vp.process("../assets/clip.mp4")
